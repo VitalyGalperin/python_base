46ba09e6ee6d7348da3c727ef101b9a3fbc2dd8d
Vitaly Galperin <vitalygl@yandex.ru>
2020-01-20 19:24:40 +0200

    Задание 00 bubbles

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -1,24 +1,56 @@
 # -*- coding: utf-8 -*-
+import random
 
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
+point = sd.get_point(100, 100)
+radius = 50
+for _ in range(3):
+    radius += 5
+    sd.circle(center_position=point, radius=radius, width=2)
+
+sd.user_want_exit(1)
+sd.clear_screen()
+
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+def bubble(point, step):
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(center_position=point, radius=radius, color=sd.random_color())
+
+
+point = sd.get_point(100, 100)
+bubble(point=point, step=10)
+
+sd.user_want_exit(1)
+sd.clear_screen()
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+for x in range(100, 1001, 100):
+    point = sd.get_point(x, 100)
+    bubble(point=point, step=5)
+
+sd.user_want_exit(1)
+sd.clear_screen()
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+for y in range(100, 301, 100):
+    for x in range(100, 1001, 100):
+        point = sd.get_point(x, y)
+        bubble(point=point, step=5)
+
+sd.user_want_exit(1)
+sd.clear_screen()
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+for _ in range(100):
+    point = sd.random_point()
+    step=random.randint(2,10)
+    bubble(point=point, step=step)
 
 sd.pause()
-
-
