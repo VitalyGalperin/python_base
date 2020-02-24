83139e5cf6617645a7b78bd060100ace9cf26499
Vitaly Galperin <vitalygl@yandex.ru>
2020-02-24 10:42:50 +0200

    урок 07 - 01 исправлены замечания

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -14,31 +14,30 @@ sd.resolution = (900, 600)
 class Snowflake:
 
     def __init__(self):
-        x_coordinate = sd.random_number(0, 900)
-        y_coordinate = 300 + sd.random_number(0, 600)
-        length = sd.random_number(3, 20)
-        place_of_rays = sd.random_number(1, 10) * 0.1
-        ray_length = sd.random_number(1, 50) * 0.01
-        deflection_angle = sd.random_number(1, 90)
-        # TODO Вам не нужна данный список, его необходимо удалить и обращаться
-        #  в коде напрямую к нужным атрибутам класса
-        self.snow = [x_coordinate, y_coordinate, length, place_of_rays, ray_length, deflection_angle]
+        self.x_coordinate = sd.random_number(0, 900)
+        self.y_coordinate = 300 + sd.random_number(0, 600)
+        self.length = sd.random_number(3, 20)
+        self.place_of_rays = sd.random_number(1, 10) * 0.1
+        self.ray_length = sd.random_number(1, 50) * 0.01
+        self.deflection_angle = sd.random_number(1, 90)
 
     def clear_previous_picture(self):
-        sd.snowflake(center=sd.get_point(self.snow[0], self.snow[1]), length=self.snow[2],
-                     factor_a=self.snow[3], factor_b=self.snow[4], factor_c=self.snow[5], color=sd.background_color)
+        sd.snowflake(center=sd.get_point(self.x_coordinate, self.y_coordinate), length=self.length,
+                     factor_a=self.place_of_rays, factor_b=self.ray_length, factor_c=self.deflection_angle,
+                     color=sd.background_color)
 
     def move(self):
-        if self.snow[1] > 0:
-            self.snow[0] = self.snow[0] + sd.random_number(-3, 3)
-            self.snow[1] -= 1
+        if self.y_coordinate > 0:
+            self.x_coordinate += sd.random_number(-3, 3)
+            self.y_coordinate -= 1
 
     def draw(self):
-        sd.snowflake(center=sd.get_point(self.snow[0], self.snow[1]), length=self.snow[2],
-                     factor_a=self.snow[3], factor_b=self.snow[4], factor_c=self.snow[5], color=sd.COLOR_WHITE)
+        sd.snowflake(center=sd.get_point(self.x_coordinate, self.y_coordinate), length=self.length,
+                     factor_a=self.place_of_rays, factor_b=self.ray_length, factor_c=self.deflection_angle,
+                     color=sd.COLOR_WHITE)
 
     def can_fall(self):
-        return self.snow[1]
+        return self.y_coordinate
 
 
 def get_flakes(snow_numbers):
@@ -48,10 +47,10 @@ def get_flakes(snow_numbers):
     return snows
 
 
-def get_fallen_flakes():
+def get_fallen_flakes(flakes):
     count_fallen_flakes = 0
     for snow in flakes:
-        if snow.snow[1] <= 0:
+        if snow.y_coordinate <= 0:
             flakes.remove(snow)
             count_fallen_flakes += 1
     return count_fallen_flakes
@@ -83,7 +82,7 @@ while True:
         flake.clear_previous_picture()
         flake.move()
         flake.draw()
-    fallen_flakes = get_fallen_flakes()  # подчитать сколько снежинок уже упало
+    fallen_flakes = get_fallen_flakes(flakes)  # подчитать сколько снежинок уже упало
     if fallen_flakes > 0:
         append_flakes(count=fallen_flakes)  # добавить еще сверху
     # sd.sleep(0.1)
