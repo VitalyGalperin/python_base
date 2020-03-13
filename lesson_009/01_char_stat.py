d2c90e7a9cf79c4ae28a91cbffff124b6fb7b44e
Vitaly Galperin <vitalygl@yandex.ru>
2020-03-13 20:48:16 +0200

    урок 09 1 статистика. Ё выпадает из сортировки

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -21,10 +21,74 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
-
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
 #  - по алфавиту по возрастанию
 #  - по алфавиту по убыванию
 # Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
+
+import zipfile
+
+
+class Chatterer:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+        self.printing_stat = ()
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def collect(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                self._collect_for_line(line=line[:-1])
+        self.printing_stat = list(self.stat.items())
+
+    def _collect_for_line(self, line):
+        for char in line:
+            if char.isalpha():
+                if char in self.stat:
+                    self.stat[char] += 1
+                else:
+                    self.stat[char] = 1
+
+    def sort_statistic(self, key='alphabet', reverse=False):
+        if key == 'quantity' and not reverse:
+            self.printing_stat = sorted(self.stat.items(), key=lambda item: -item[1])
+        elif key == 'quantity' and reverse:
+            self.printing_stat = sorted(self.stat.items(), key=lambda item: item[1])
+        elif key == 'alphabet' and not reverse:
+            self.printing_stat = sorted(self.stat.items(), key=lambda item: item[0])
+        elif key == 'alphabet' and reverse:
+            self.printing_stat = sorted(self.stat.items(), key=lambda item: -ord(item[0]))
+        self._print_statistic()
+
+    def _print_statistic(self):
+        global char
+        statistic_sum = 0
+        print('+---------+----------+')
+        print('|  буква  | частота  |')
+        print('+---------+----------+')
+        for char, quantity in self.printing_stat:
+            statistic_sum += quantity
+            print(f'|    {char}    | {quantity:8d} |')
+        print('+---------+----------+')
+        print(f'|  Итого  | {statistic_sum:8d} |')
+        print('+---------+----------+')
+
+
+chatterer = Chatterer(file_name='voyna-i-mir.txt.zip')
+chatterer.collect()
+chatterer.sort_statistic(key='alphabet')
+chatterer.sort_statistic(key='alphabet', reverse=True)
+chatterer.sort_statistic(key='quantity')
+chatterer.sort_statistic(key='quantity', reverse=True)
+
+
