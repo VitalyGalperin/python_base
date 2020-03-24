64e958087cb4b7f38a97d5b00f1fa83c603365c4
Vitaly Galperin <vitalygl@yandex.ru>
2020-03-24 22:59:31 +0200

    урок 9 3 замечания исправлены

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -65,11 +65,10 @@ class FileOrder:
                 if extension in self.image_extension:
                     source_full_path = os.path.join(dirpath, file_name)
                     struct_file_date = time.gmtime(os.path.getmtime(source_full_path))
-                    struct_path = 'icons_by_year/' + str(struct_file_date.tm_year) + '/' + str(
-                        struct_file_date.tm_mon) + '/'   # TODO os.path.join - для переносимости, у меня windows например,
-                    if not os.path.isdir(struct_path):  # TODO makedirs имеет параметр
-                        os.makedirs(struct_path)
-                    recipient_full_path = struct_path + file_name
+                    struct_path = os.path.join('icons_by_year', str(struct_file_date.tm_year),
+                                               str(struct_file_date.tm_mon))
+                    os.makedirs(struct_path, exist_ok=True)
+                    recipient_full_path = os.path.join(struct_path, file_name)
                     if not os.path.isfile(recipient_full_path):
                         copy2(source_full_path, recipient_full_path)
 
@@ -78,11 +77,9 @@ class FileOrder:
         for file_name in source_zip.infolist():
             name, extension = os.path.splitext(file_name.filename)
             if extension in self.image_extension:
-                target_path = 'icons_by_year/' + str(file_name.date_time[0]) + '/' + str(
-                    file_name.date_time[1]) + '/'  # TODO os.path.join
-                target_file = target_path + os.path.split(name)[1] + extension
-                if not os.path.isdir(target_path): # TODO makedirs имеет параметр
-                    os.makedirs(target_path)
+                target_path = os.path.join('icons_by_year', str(file_name.date_time[0]), str(file_name.date_time[1]))
+                target_file = os.path.join(target_path, os.path.split(name)[1] + extension)
+                os.makedirs(target_path, exist_ok=True)
                 if not os.path.isfile(target_file):
                     with source_zip.open(file_name.filename) as source, open(target_file, 'wb') as target:
                         copyfileobj(source, target)
