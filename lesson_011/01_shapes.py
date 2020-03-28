5d58c5b3e3b67915749be3885a4f6f40742b8d81
Vitaly Galperin <vitalygl@yandex.ru>
2020-03-28 13:34:20 +0300

    урок 11 1

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -14,12 +14,17 @@ import simple_draw as sd
 
 
 def get_polygon(n):
-    pass
-    # TODO здесь ваш код
+    def figure_drawing(point=sd.get_point(200, 200), angle=13, length=100):
+        current_point = point
+        for i in range(n):
+            vector = sd.get_vector(start_point=current_point, angle=angle + 360 / n * i, length=length)
+            vector.draw()
+            current_point =vector.end_point
+        sd.line(start_point=point, end_point=current_point)
+    return figure_drawing
 
 
 draw_triangle = get_polygon(n=3)
 draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
-
 
 sd.pause()
