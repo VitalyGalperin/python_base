4e3b5dca2a6cecf61f051a1862c1a6fdb968baee
Vitaly Galperin <vitalygl@yandex.ru>
2020-04-07 19:37:08 +0300

    урок 12 вторая часть, кроме join при 0 волатильности

--- lesson_012/01_volatility.py
+++ lesson_012/01_volatility.py
@@ -65,18 +65,94 @@
 #
 # Для плавного перехода к мультипоточности, код оформить в обьектном стиле, используя следующий каркас
 #
+# import os
+#
+#
+# class FileHandler:
+#     def __init__(self, full_path):
+#         self.full_path = full_path
+#
+#     def processing(self):
+#         with open(self.full_path, mode='r', encoding='utf8') as file:
+#             min_price = max_price = 0
+#             for line in file:
+#                 ticker, time, price, volume = line.split(',')
+#                 try:
+#                     price = float(price)
+#                 except ValueError:
+#                     continue
+#                 if not min_price or price < min_price:
+#                     min_price = price
+#                 if price > max_price:
+#                     max_price = price
+#             average_price = (max_price + min_price) / 2
+#             try:
+#                 volatility = ((max_price - min_price) / average_price) * 100
+#             except ZeroDivisionError:
+#                 volatility = 0
+#             return ticker, volatility
+#
+#
+# class VolatilityMeter:
+#
+#     def __init__(self):
+#         self.path = self._make_path()
+#         self.dirpath = self.dirnames = self.filenames = ''
+#         self.tickers = {}
+#
+#     def run(self):
+#         for self.dirpath, self.dirnames, self.filenames in os.walk(self.path):
+#             for filename in self.filenames:
+#                 full_path = os.path.join(self.dirpath, filename)
+#                 file = FileHandler(full_path)
+#                 ticker, volatility = file.processing()
+#                 self.tickers[ticker] = volatility
+#             self._print_max_tickers(tickers_number=3)
+#             self._print_min_tickers(tickers_number=3)
+#             self._print_zero_tickers()
+#
+#     def _make_path(self):
+#         return os.path.join(os.getcwd(), 'trades')
+#
+#     def _print_max_tickers(self, tickers_number=0):
+#         print('Максимальная волатильность:')
+#         for ticker, volatility in sorted(self.tickers.items(), key=lambda x: x[1], reverse=True)[:tickers_number]:
+#             print(f'    {ticker} -{volatility:6.2f} %')
+#
+#     def _print_min_tickers(self, tickers_number=0):
+#         print('Минимальная волатильность:')
+#         filtered_tickers = filter(lambda x: x[1] != 0, self.tickers.items())
+#         for ticker, volatility in sorted(filtered_tickers, key=lambda x: x[1])[:tickers_number]:
+#             print(f'    {ticker} -{volatility:6.2f} %')
+#
+#     def _print_zero_tickers(self):
+#         print('Нулевая волатильность:')
+#         filtered_tickers = filter(lambda x: x[1] == 0, self.tickers.items())
+#         print_tickers = ''
+#         print(' '.join(self.tickers.keys()))
+#         for ticker, volatility in sorted(filtered_tickers, key=lambda x: x[0]):
+#             print_tickers += ticker + ' '  # TODO используйте ' '.join
+#         print(print_tickers)
+#
+#
+# volatility_handler = VolatilityMeter()
+# volatility_handler.run()
+
+
+# Зачет
+
+from threading import Thread
 import os
 
 
-# TODO код нужно разделить, для того чтобы потом удобно перейти к много поточности. Отдельный класс,
-#  который обрабатывает один файл, и основной код получения списка файлов и вывода результата
-
-
-class FileHandler:
-    def __init__(self, full_path):
+class FileHandler(Thread):
+    def __init__(self, full_path, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.full_path = full_path
+        self.ticker = ''
+        self.volatility = 0
 
-    def processing(self):
+    def run(self):
         with open(self.full_path, mode='r', encoding='utf8') as file:
             min_price = max_price = 0
             for line in file:
@@ -94,7 +170,7 @@ class FileHandler:
                 volatility = ((max_price - min_price) / average_price) * 100
             except ZeroDivisionError:
                 volatility = 0
-            return ticker, volatility
+            self.ticker, self.volatility = ticker, volatility
 
 
 class VolatilityMeter:
@@ -103,14 +179,23 @@ class VolatilityMeter:
         self.path = self._make_path()
         self.dirpath = self.dirnames = self.filenames = ''
         self.tickers = {}
+        self.file_treads = []
 
-    def run(self):
+    def process(self):
+
         for self.dirpath, self.dirnames, self.filenames in os.walk(self.path):
+            file_list = []
             for filename in self.filenames:
                 full_path = os.path.join(self.dirpath, filename)
-                file = FileHandler(full_path)
-                ticker, volatility = file.processing()
-                self.tickers[ticker] = volatility
+                file_list.append(full_path)
+            for file in file_list:
+                self.file_treads.append(FileHandler(file))
+            for tread in self.file_treads:
+                tread.start()
+            for tread in self.file_treads:
+                tread.join()
+            for tread in self.file_treads:
+                self.tickers[tread.ticker] = tread.volatility
             self._print_max_tickers(tickers_number=3)
             self._print_min_tickers(tickers_number=3)
             self._print_zero_tickers()
@@ -133,11 +218,11 @@ class VolatilityMeter:
         print('Нулевая волатильность:')
         filtered_tickers = filter(lambda x: x[1] == 0, self.tickers.items())
         print_tickers = ''
+        print(' '.join(self.tickers.keys()))
         for ticker, volatility in sorted(filtered_tickers, key=lambda x: x[0]):
             print_tickers += ticker + ' '  # TODO используйте ' '.join
         print(print_tickers)
 
 
 volatility_handler = VolatilityMeter()
-volatility_handler.run()
-# Зачет
+volatility_handler.process()
