5836dec079f5b2974523075aafd3adc3983a5538
Vitaly Galperin <vitalygl@yandex.ru>
2020-04-12 17:07:09 +0300

    ChatBot. перевод по умолчанию. с IF

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -4,6 +4,7 @@ from _token import token
 import vk_api
 import vk_api.bot_longpoll
 import random
+from translate import Translator
 
 group_id = 194114072
 
@@ -19,18 +20,35 @@ class Bot:
     def run(self):
         for event in self.long_poller.listen():
             try:
+                print(event)
                 self.on_event(event)
             except Exception as err:
                 print(err)
 
     def on_event(self, event):
         if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
-            print(event.object.text)
-            self.api.messages.send(message=event.object.text,
-                                   random_id=random.randint(0, 2 ** 20),
-                                   peer_id=event.object.peer_id,)
-        else:
-            print(event)
+            if not event.object.text:
+                a = 'В сообщении отсутствует текст'
+                self.on_send(event, a)
+            elif len(event.object.text) > 499:
+                a = 'Максимальная длина текста для перевода: 500 знаков'
+                self.on_send(event, a)
+            else:
+                a = self.on_translate(event)
+                self.on_send(event, a)
+
+    def on_translate(self, event, from_lang='ru', to_lang='en'):
+        translator = Translator(to_lang, from_lang)
+        try:
+            translation = translator.translate(event.object.text)
+        except Exception as err:
+            translation = 'Перевод не удался'
+        return translation
+
+    def on_send(self, event, a):
+        self.api.messages.send(message=a,
+                               random_id=random.randint(0, 2 ** 20),
+                               peer_id=event.object.peer_id, )
 
 
 if __name__ == "__main__":
