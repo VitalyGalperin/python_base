03cf18fe312ad07ff3b884aef837a0de7d7be624
Vitaly Galperin <vitalygl@yandex.ru>
2020-04-14 13:56:01 +0300

    урок 13 заполнение билета Дополнительное задание в ticket.py

--- lesson_013/ticket.py
+++ lesson_013/ticket.py
@@ -13,8 +13,10 @@ import os
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 
 
-def make_ticket(fio, from_, to, date):
+def make_ticket(fio, from_, to, date, save_to=None):
     ticket_path = os.path.join(os.getcwd(), 'images')
+    if not save_to:
+        save_to = ticket_path
     ticket_blank = 'ticket_template.png'
     ticket_result = 'ticket.png'
     ticket = Image.open(os.path.join(ticket_path, ticket_blank))
@@ -41,11 +43,11 @@ def make_ticket(fio, from_, to, date):
     field = f'{date}'
     draw.text((x, y), field, font=font, fill=ImageColor.colormap['black'])
 
-    ticket.save(os.path.join(ticket_path, ticket_result))
+    ticket.save(os.path.join(save_to, ticket_result))
 
 
-if __name__ == '__main__':
-    make_ticket(fio='ГАЛЬПЕРИН В.А.', from_='ЭЙЛАТ (IL)', to='МОСКВА (RU)', date='20.07.2020')
+# if __name__ == '__main__':
+#     make_ticket(fio='ГАЛЬПЕРИН В.А.', from_='ЭЙЛАТ (IL)', to='МОСКВА (RU)', date='20.07.2020')
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля agrparse.
@@ -56,3 +58,13 @@ if __name__ == '__main__':
 #   --date - обязательный, когда летим.
 #   --save_to - необязательный, путь для сохранения заполненнего билета.
 # и заполнять билет.
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fio', required=True, help='фамилия', dest='fio')
+    parser.add_argument('--from', required=True, help='откуда летим', dest='from_')
+    parser.add_argument('--to', required=True, help='куда летим', dest='to')
+    parser.add_argument('--date', required=True, help='дата вылета', dest='date')
+    parser.add_argument('--save_to', required=False, help='путь для сохранения заполненнего билета', dest='save_to')
+    args = parser.parse_args()
+    make_ticket(args.fio, args.from_, args.to, args.date, args.save_to)
