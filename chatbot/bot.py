9f0a3d31f7412530211d5e0bf6dbf0281d6f57e4
Vitaly Galperin <vitalygl@yandex.ru>
2020-06-08 21:10:51 +0300

    ChatBot 15 задание. Из лекции. без тестов

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -8,6 +8,7 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import logging
 import random
 import settings
+import handlers
 
 log = logging.getLogger('bot')
 
@@ -23,6 +24,12 @@ def config_log():
     log.addHandler(file_handler)
     log.setLevel(logging.DEBUG)
 
+class UserState:
+
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or {}
 
 class Bot:
     """
@@ -71,8 +78,9 @@ class Bot:
             text_to_send = self.continue_scenario(user_id, text)
         else:
             # search intevt
-            for intent in self.user_states:
-                if any(token in text for token in intent['tokens']):
+            for intent in settings.INTENTS:
+                log.debug(f'user get {intent}')
+                if any(token in text.lower() for token in intent['tokens']):
                     if intent['answer']:
                         text_to_send = intent['answer']
                     else:
@@ -81,26 +89,43 @@ class Bot:
             else:
                 text_to_send = settings.DEFAULT_ANSWER
 
-            self.api.messages.send(
-                message=text_to_send,
-                random_id=random.randint(0, 2 ** 20),
-                peer_id=event.object.peer_id, )
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=random.randint(0, 2 ** 20),
+            peer_id=event.object.peer_id, )
 
     def start_scenario(self, user_id, scenario_name):
         scenario = settings.SCENARIOS[scenario_name]
         first_step = scenario['first_step']
-        step = scenario['step'][first_step]
+        step = scenario['steps'][first_step]
         text_to_send = step['text']
         self.user_states[user_id] =UserState(scenario_name=scenario_name, step_name=first_step)
-
-        return True
+        return text_to_send
 
     def continue_scenario(self, user_id, text):
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
 
-        return True
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                #swith to next step
+                state.step_name = step['next_step']
+            else:
+                #finish scenario
+                self.user_states.pop(user_id)
+                log.info('зарегистрирован: {name} {email}'.format(**state.context))
+        else:
+            #retry current step
+            text_to_send = step['failure_text']
+
+        return text_to_send
 
 
 if __name__ == "__main__":
     config_log()
-    bot = Bot(GROUP_ID, TOKEN)
+    bot = Bot(settings.GROUP_ID, settings.TOKEN)
     bot.run()
