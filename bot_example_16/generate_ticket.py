83ee88ed3735b6bce32d54c0f0515b3e3089e4cc
Vitaly Galperin <vitalygl@yandex.ru>
2020-09-22 17:42:27 +0300

    убрал файлы результатов из заданий 7,9,10,11,13,14

--- bot_example_16/generate_ticket.py
+++ bot_example_16/generate_ticket.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+import requests
 from PIL import Image, ImageFont, ImageDraw
 
 TEMPLATE_PATH = 'files/ticket_base.png'
@@ -8,6 +10,9 @@ BLACK = (0, 0, 0, 255)
 NAME_OFFSET = (250, 220)
 EMAIL_OFFSET = (250, 450)
 
+AVATAR_SIZE = 180
+AVATAR_OFFSET = (10, 10)
+
 
 def generate_ticket(name, email):
     base = Image.open(TEMPLATE_PATH).convert('RGBA')
@@ -17,7 +22,21 @@ def generate_ticket(name, email):
     draw.text(NAME_OFFSET, name, font=font, fill=BLACK)
     draw.text(EMAIL_OFFSET, email, font=font, fill=BLACK)
 
-    base.show()
+    response = requests.get(url=f'https://api.adorable.io/avatars/{AVATAR_SIZE}/{email}')
+    avatar_file_like = BytesIO(response.content)
+    avatar = Image.open(avatar_file_like)
+    base.paste(avatar, AVATAR_OFFSET)
 
+    # Пример как сохранять в файл
+    # with open('files/ticket_example.png', 'wb') as file:
+    #     base.save(file, 'png')
 
-generate_ticket('Vasily', 'vasily@ghj.kjn')
+    # Пример как сохранять в виртуальный файл
+    temp_file = BytesIO()
+    base.save(temp_file, 'png')
+    temp_file.seek(0)           # возврат курсора в точку 0
+
+    return temp_file
+
+# Вызов самой себя для теста
+# generate_ticket('Vasily', 'vasily@ghj.kjn')
