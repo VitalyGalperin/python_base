8a3592c1b1835878ac52c074521ee05745c89376
Vitaly Galperin <vitalygl@yandex.ru>
2020-10-06 21:21:42 +0300

    16 задание. Основа рисования открытки. начало запускающего модумя

--- lesson_016/01_weather_run.py
+++ lesson_016/01_weather_run.py
@@ -13,6 +13,6 @@ class WeatherHandler():
 if __name__ == "__main__":
     weather = WeatherMaker()
     nn_weather = weather.darksky_parsing('56.3268,44.0058', '2019-11-14')  # NN
-    # weather.darksky_parsing('29.5563,34.9525', '2020-10-5')   # Eilat ...
+    # weather.darksky_parsing('29.5563,34.9525', '2020-10-5')   # Eilat ....
     image = ImageMaker()
     image.run()
