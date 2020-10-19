10c0897a25b8b1477bc0be3eb3b0fe05139625cd
Vitaly Galperin <vitalyglll@gmail.com>
2020-10-19 17:56:38 +0300

    16 задание.Включил в репозиторий файл set.py

---
+++ lesson_016/set.py
@@ -0,0 +1,23 @@
+import re
+
+re_date = re.compile(
+    r'^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$')
+
+OpenMapQuest_API_KEY = 'kvDuJJUTE50Ax5XG8mxbCVDGnQqHFdvL'
+
+DB_URL = "sqlite:///weather.db"
+
+IMAGE_DIR = 'image_data'
+BLANK_FILE = 'blank.jpg'
+RESULT_FILE = 'card.jpg'
+
+RAIN_ICON = 'rain.jpg'
+SNOW_ICON = 'snow.jpg'
+DRIZZLE_ICON = 'drizzle.jpg'
+CLOUDY_ICON = 'cloudy.jpg'
+OVERCAST_ICON = 'overcast.jpg'
+FOGGY_ICON = 'foggy.jpg'
+SUNNY_ICON = 'sun.jpg'
+SLEET_ICON = 'sleet.jpg'
+
+FONT_COLOR = (20, 20, 0)
